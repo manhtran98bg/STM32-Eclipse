b5d498615ed9e38e43d72fa0a225018f388e22e7
manh.th200298 <manhtran98bg@gmail.com>
2021-04-13 11:03:13 +0700

    GPS function

--- STM32_Tracking_GPS/src/gps/gps.c
+++ STM32_Tracking_GPS/src/gps/gps.c
@@ -6,6 +6,8 @@
  */
 
 #include "gps.h"
+__IO uint8_t flagStart = 0,flagStop = 0;
+extern __IO uint8_t RxBuffer4[];
 
 void gps_power_on()
 {
@@ -15,4 +17,52 @@ void gps_power_off()
 {
 	power_off_gps();
 }
-
+uint8_t  gps_read_data()
+{
+	if (flagStop)
+	{
+#if _DEBUG
+		trace_puts((char*)RxBuffer4);
+#endif
+#if _DEBUG_GPS_UART5
+		UART5_Send_String((uint8_t*)RxBuffer4);
+		USART_SendData(UART5, '\n');
+#endif
+		USART_clear_buf(4);
+		flagStop=0;
+		return 1;
+	}
+	return 0;
+}
+void gps_reset()
+{
+	GPIO_SetBits(GPS_RST_PORT, GPS_RST_PIN);
+	delay_ms(20);
+	GPIO_ResetBits(GPS_RST_PORT, GPS_RST_PIN);
+}
+static void gps_rst_pin_init()
+{
+	RCC_APB2PeriphClockCmd(GPS_RST_CLK, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Pin = GPS_RST_PIN;
+	GPIO_InitStruct.GPIO_Mode= GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPS_RST_PORT, &GPIO_InitStruct);
+	GPIO_ResetBits(GPS_RST_PORT, GPS_RST_PIN);
+}
+static uint8_t gps_check_cmd(uint8_t * cmd, __IO uint8_t * response)
+{
+	char* res;
+	UART4_Send_String(cmd);
+	res = strstr((char*)response,(char*)cmd);
+	if (res) return 1;
+	else return 0;
+}
+void gps_init()
+{
+	gps_rst_pin_init();
+	gps_power_on();
+	delay_ms(1000);
+	UART4_Send_String((uint8_t*)"$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n");
+	USART_clear_buf(4);
+}
--- STM32_Tracking_GPS/src/gps/gps.h
+++ STM32_Tracking_GPS/src/gps/gps.h
@@ -10,5 +10,16 @@
 
 #include "main.h"
 #include "../power/power.h"
+#include "../usart/usart.h"
 #define _DEBUG_GPS_UART5 1
+
+#define GPS_RST_CLK	RCC_APB2Periph_GPIOC
+#define GPS_RST_PORT	GPIOC
+#define GPS_RST_PIN		GPIO_Pin_5
+
+void gps_power_on();
+void gps_power_off();
+void gps_reset();
+uint8_t gps_read_data();
+void gps_init();
 #endif /* GPS_GPS_H_ */
